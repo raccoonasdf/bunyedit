509fd8575014f21d3d547594fbe670e3d734944d
raccoon! <raccoon@raccoon.fun>
2024-02-20 16:48:27 -0500

    Add more chunkpos math

--- src/main/java/fun/raccoon/bunyedit/util/PosMath.java
+++ src/main/java/fun/raccoon/bunyedit/util/PosMath.java
@@ -1,11 +1,12 @@
 package fun.raccoon.bunyedit.util;
 
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 import net.minecraft.core.world.chunk.ChunkPosition;
 
 public class PosMath {
-    public static ChunkPosition mapComponents(
+    public static ChunkPosition mapPositions(
         BiFunction<Integer, Integer, Integer> f,
         ChunkPosition a, ChunkPosition b
     ) {
@@ -15,11 +16,33 @@ public class PosMath {
             f.apply(a.z, b.z));
     }
 
+    public static ChunkPosition mapPosition(
+        Function<Integer, Integer> f,
+        ChunkPosition a
+    ) {
+        return new ChunkPosition(
+            f.apply(a.x),
+            f.apply(a.y),
+            f.apply(a.z));
+    }
+
     public static ChunkPosition add(ChunkPosition a, ChunkPosition b) {
-        return mapComponents(((x, y) -> x + y), a, b);
+        return mapPositions(((x, y) -> x + y), a, b);
     }
 
     public static ChunkPosition sub(ChunkPosition a, ChunkPosition b) {
-        return mapComponents(((x, y) -> x - y), a, b);
+        return mapPositions(((x, y) -> x - y), a, b);
+    }
+
+    public static ChunkPosition mul(ChunkPosition a, ChunkPosition b) {
+        return mapPositions(((x, y) -> x * y), a, b);
+    }
+
+    public static ChunkPosition abs(ChunkPosition a) {
+        return mapPosition((x -> Math.abs(x)), a);
+    }
+
+    public static ChunkPosition all(int x) {
+        return new ChunkPosition(x, x, x);
     }
 }
